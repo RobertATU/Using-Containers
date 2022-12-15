de50a3fdbf0385e4e1f35451836f157831421449
Robert <g00395612@atu.ie>
2022-12-15 16:02:32 +0000

    Added another map

--- src/main/java/ie/atu/Using/Containers/PassengerController.java
+++ src/main/java/ie/atu/Using/Containers/PassengerController.java
@@ -1,6 +1,7 @@
 package ie.atu.Using.Containers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +17,10 @@ public class PassengerController {
                 new Passenger("Mr", "Paul3", "1234567890", "55624564525", 53));
         return myPassengers;
     }
+    @GetMapping("/{passengerID}")
+    public Passenger getPassenger(@PathVariable String passengerID){
+        Passenger myPassenger =new Passenger("Mr", "Paul", "1234567890", "2123455678", 26);
+        return myPassenger;
+    }
+
 }
