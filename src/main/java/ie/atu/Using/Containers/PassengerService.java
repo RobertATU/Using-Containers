338f75459185cdbf5126028daf300bd5cf6ed0ee
Robert <g00395612@atu.ie>
2022-12-15 15:36:33 +0000

    Added Passenger Controller and Service

---
+++ src/main/java/ie/atu/Using/Containers/PassengerService.java
@@ -0,0 +1,5 @@
+package ie.atu.Using.Containers;
+
+public class PassengerService {
+
+    }
--- src/main/java/ie/atu/Using/Containers/UsingContainersApplication.java
+++ src/main/java/ie/atu/Using/Containers/UsingContainersApplication.java
@@ -1,13 +1,5 @@
 package ie.atu.Using.Containers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class UsingContainersApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UsingContainersApplication.class, args);
-	}
 
 }
