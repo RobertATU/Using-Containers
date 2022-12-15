aac214b99bbf1f8243bbbfdfb105a6bdf833a013
Robert <g00395612@atu.ie>
2022-12-15 16:15:07 +0000

    Divided functionality in service and controller class

--- src/main/java/ie/atu/Using/Containers/PassengerController.java
+++ src/main/java/ie/atu/Using/Containers/PassengerController.java
@@ -9,18 +9,22 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/passenger")
 public class PassengerController {
+    public PassengerController(PassengerService myService) {
+        this.myService = myService;
+    }
+
+    PassengerService myService;
+
     @GetMapping
     public List<Passenger> getPassengers() {
-        List<Passenger> myPassengers = List.of(
-                new Passenger("Mr", "Paul", "1234567890", "2123455678", 23),
-                new Passenger("Mr", "Paul2", "1234567890", "2485665567", 93),
-                new Passenger("Mr", "Paul3", "1234567890", "55624564525", 53));
-        return myPassengers;
+
+
+        return myService.getPassengers();
     }
     @GetMapping("/{passengerID}")
     public Passenger getPassenger(@PathVariable String passengerID){
-        Passenger myPassenger =new Passenger("Mr", "Paul", "1234567890", "2123455678", 26);
-        return myPassenger;
+
+        return myService.getPassenger(passengerID);
     }
 
 }
--- src/main/java/ie/atu/Using/Containers/PassengerService.java
+++ src/main/java/ie/atu/Using/Containers/PassengerService.java
@@ -1,8 +1,26 @@
 package ie.atu.Using.Containers;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Service
 public class PassengerService {
+    @GetMapping
+    public List<Passenger> getPassengers() {
+        List<Passenger> myPassengers = List.of(
+                new Passenger("Mr", "Paul", "1234567890", "2123455678", 23),
+                new Passenger("Mr", "Paul2", "1234567890", "2485665567", 93),
+                new Passenger("Mr", "Paul3", "1234567890", "55624564525", 53));
+        return myPassengers;
+    }
+
+    public Passenger getPassenger(String passengerID){
+        Passenger myPassenger =new Passenger("Mr", "Paul", "1234567890", "2123455678", 26);
+        return myPassenger;
+    }
 
     }
