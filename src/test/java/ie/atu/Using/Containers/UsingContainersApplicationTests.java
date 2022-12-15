4b2f419c2ae3f73c7757768dd522eb18906b2ebe
Robert <g00395612@atu.ie>
2022-12-15 14:55:11 +0000

    First commit

---
+++ src/test/java/ie/atu/Using/Containers/UsingContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.Using.Containers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
